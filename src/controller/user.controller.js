864ccd09ba1c23ec306ace944697aaeeaf7f159a
Hung Pham <phamphihungbk@gmail.com>
2021-12-02 22:55:32 +0700

    fix: user controller

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,11 +1,11 @@
-import userRepo from '../repository/user.repository';
+import userRepo from '../repository/user.repository.js';
 
 /**
  * Delete user.
- * @returns {User}
+ * @returns {string}
  */
 async function remove(req, res) {
-	const userId = req.body.id;
+	const userId = req.params.id;
 	await userRepo.deleteItem(userId);
 
 	return res.status(200).json({ message: 'Successfully deleted' })
@@ -13,25 +13,25 @@ async function remove(req, res) {
 
 /**
  * Update existing user
- * @property {string} req.body.name - The username of user.
- * @returns {User}
+ * @returns {User, string}
  */
 async function update(req, res) {
-	const userId = req.body.id;
-	const data = req.body.data;
-	await userRepo.updateItem(userId, data);
+	const userId = req.params.id;
+	const updateInfo = req.body;
+	const data = await userRepo.updateItem(userId, updateInfo);
 
-	return res.status(200).json({ message: 'Successfully updated' })
+	return res.status(200).json({data:data, message: 'Successfully updated' })
 }
 
 /**
  * Get user list.
  * @property {number} req.query.limit - Limit number of users to be returned.
- * @returns {User[]}
+ * @returns {[User], string}
  */
 async function list(req, res) {
 	const query = {
-		limit: (req.body.limit > 0) ? req.body.limit : 20
+		offset: req.query.offset ?? 0,
+		limit: req.query.limit ?? 20,
 	};
 	const data = await userRepo.getAllItems(query);
 
@@ -40,10 +40,10 @@ async function list(req, res) {
 
 /**
  * Get user
- * @returns {User}
+ * @returns {User, string}
  */
 async function get(req, res) {
-	const userId = req.body.id;
+	const userId = req.params.id;
 	const data = await userRepo.getItem(userId);
 
 	return res.status(200).json({ data: data, message: 'Successfully found' })
@@ -52,15 +52,15 @@ async function get(req, res) {
 /**
  * Create new user
  * @property {string} req.body.name - The username of user.
- * @returns {User}
+ * @returns {User, string}
  */
 async function create(req, res) {
 	const user = {
 		name: req.body.name,
 	};
-	await userRepo.createItem(user);
+	const data = await userRepo.createItem(user);
 
-	return res.status(201).json({ message: 'Successfully created' })
+	return res.status(201).json({ data: data, message: 'Successfully created' })
 }
 
 export default { create, update, remove, list, get };
